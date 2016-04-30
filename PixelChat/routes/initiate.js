c352dd12c90d5bee68799c4f54ad41df7563d28a
Pee T Lim <plim@Travelerss-MacBook-Pro-3.local>
2016-04-30 10:46:54 -0400

    add wit client

--- PixelChat/routes/initiate.js
+++ PixelChat/routes/initiate.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+const Wit = require('node-wit').Wit;
+const WIT_TOKEN = 'I3YIED2VEPKOWTI42FXITQOMLEOWJ544';
+
+const actions = {
+  say(sessionId, context, message, cb) {
+    console.log(message);
+    
+  },
+  merge(sessionId, context, entities, message, cb) {
+    
+    
+  },
+  error(sessionId, context, error) {
+    console.log(error.message);
+  },
+};
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const witcontext = {};
+  
+  
+  var client = new Wit(WIT_TOKEN,actions);
+  console.log("Gothere");
+  const context = {};
+  client.message('what is the weather in London?', 
+      witcontext, 
+      function(error, data) {
+        if (error) {
+          console.log('Oops! Got an error: ' + error);
+          res.send("Error");
+        } else {
+          res.send(JSON.stringify(data));
+          console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
+        }
+      });
+
 });
 
+router.post('/intent',function(req, res, next) {
+    
+});
 
 module.exports = router;
